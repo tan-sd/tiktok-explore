8953747c8723a7e2fe8d5cf9b41d9aefeab74412
tan-sd <shengdatan@gmail.com>
2023-09-03 12:31:55 +0800

    Configure Navigation

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import HomePage from "./screens/HomePage";
+import ShopPage from "./screens/ShopPage";
+import InboxPage from "./screens/InboxPage";
+import ProfilePage from "./screens/ProfilePage";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function TabNavigator() {
+    return (
+        <Tab.Navigator>
+            <Tab.Screen
+                name="Home"
+                component={HomePage}
+                options={{
+                    headerShown: false,
+                }}
+            />
+            <Tab.Screen
+                name="Shop"
+                component={ShopPage}
+                options={{
+                    headerShown: false,
+                }}
+            />
+            <Tab.Screen
+                name="Inbox"
+                component={InboxPage}
+                options={{
+                    headerShown: false,
+                }}
+            />
+            <Tab.Screen
+                name="Profile"
+                component={ProfilePage}
+                options={{
+                    headerShown: false,
+                }}
+            />
+        </Tab.Navigator>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+    return (
+        <>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen
+                        name="Tab"
+                        component={TabNavigator}
+                        options={{ headerShown: false }}
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </>
+    );
+}
