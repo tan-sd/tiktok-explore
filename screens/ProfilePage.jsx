373083c473b8a8e203d3bbb3c039e54988839633
rrachea <rrachelsng@gmail.com>
2023-09-05 12:41:54 +0800

    top section without icons

--- screens/ProfilePage.jsx
+++ screens/ProfilePage.jsx
@@ -1,35 +1,156 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View , TouchableOpacity} from "react-native";
+import ModalDropdown from 'react-native-modal-dropdown';
+import React, { useState } from 'react';
 
-export default function HomePage() {
+export default function ProfilePage() {
+
+    const [selectedUser, setSelectedUser] = useState('Adam Tan');
+    const [accountName, setAccountName] = useState('dancerAdam');
+    const [activeTab, setActiveTab] = useState('Tab1');
+
+    const userToAccountMap = {
+        'Adam Tan': 'dancerAdam',
+        'Rachel': 'singerRach',
+        'Seth Yap': 'drummerSeth',
+        'Sheng Da': 'guitarCaleb',
+        'Jun Sui': 'dancerJun',
+      };
+
+    const handleSelectUser = (index, value) => {
+        setSelectedUser(value);
+        const correspondingAccountName = userToAccountMap[value];
+        setAccountName(correspondingAccountName);
+    };
+
+    // Define the content for each tab
+    const tabContent = {
+        Tab1: 'Videos',
+        Tab2: 'Private Videos',
+        Tab3: 'Collections',
+        Tab4: 'Heart shape',
+    };
+
+    // Function to switch between tabs
+    const switchTab = (tab) => {
+        setActiveTab(tab);
+    };
+
     return (
     <View style={styles.container}>
             
       <View style={styles.topProfile}>
-        <Text>Dropdown here</Text>
+
+        <ModalDropdown
+            options={['Adam Tan', 'Rachel', 'Seth Yap', 'Sheng Da', 'Jun Sui', ]} 
+            dropdownTextStyle={styles.dropdownOptionText}
+            dropdownStyle={styles.dropdownContainer}
+            onSelect={(index, value) => {
+            
+                setSelectedUser(value);
+                const correspondingAccountName = userToAccountMap[value];
+                setAccountName(correspondingAccountName);
+
+            }}
+            dropdownPosition="top" 
+        >
+        <Text style={styles.dropdownButtonText}>
+          {selectedUser ? selectedUser : 'Adam Tan'}
+        </Text>
+        </ModalDropdown>
+
+       
         {/* <Icon></Icon> */}
-        <Text>@accountname</Text>
-        <Text>Followers</Text>
+        <Text>@ {accountName ? accountName : 'dancerAdam'}</Text>
+        <View style={styles.followingContainer}>
+            <View style={styles.section}>
+                <Text>50</Text>
+                <Text>Following</Text>
+            </View>
+            <View style={styles.section}>
+                <Text>50</Text>
+                <Text>Followers</Text>
+                </View>
+            <View style={styles.section}>
+                <Text>50</Text>
+                <Text>Likes</Text>
+                </View>
+        </View>
 
         <View style={styles.buttonContainer}>
+            <TouchableOpacity
+                style={[styles.button, {marginRight:10}]}
+                onPress={() => {
+                // Handle the press of Button 1
+                }}
+            >
+            <Text style={styles.buttonText}> Edit Profile</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={styles.button}
+                onPress={() => {
+                // Handle the press of Button 2
+                }}
+            > 
+            <Text style={styles.buttonText}>Add Friends</Text>
+            </TouchableOpacity>
+        </View>
+
+        {/* bio part */}
         <TouchableOpacity
-            style={[styles.button, {marginRight:10}]}
+            style={[styles.buttonBio, {marginTop: 10}]}
             onPress={() => {
-            // Handle the press of Button 1
             }}
+            > 
+            <Text style={[styles.buttonText, {fontSize:10}]}>Add Bio</Text>
+      </TouchableOpacity>
+
+
+       {/* Row of tabs */}
+       <View style={styles.tabRow}>
+        <TouchableOpacity
+          style={[
+            styles.tabButton,
+            activeTab === 'Tab1' && styles.activeTab,
+          ]}
+          onPress={() => switchTab('Tab1')}
         >
-        <Text style={styles.buttonText}> Edit Profile</Text>
+          <Text>aaaa</Text>
         </TouchableOpacity>
         <TouchableOpacity
-            style={styles.button}
-            onPress={() => {
-            // Handle the press of Button 2
-            }}
-        > 
-        <Text style={styles.buttonText}>Add Friends</Text>
-      </TouchableOpacity>
-    </View>
-        <Text>Section 1 Content</Text>
+          style={[
+            styles.tabButton,
+            activeTab === 'Tab2' && styles.activeTab,
+          ]}
+          onPress={() => switchTab('Tab2')}
+        >
+          <Text>Tab 2</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[
+            styles.tabButton,
+            activeTab === 'Tab3' && styles.activeTab,
+          ]}
+          onPress={() => switchTab('Tab3')}
+        >
+          <Text>Tab 3</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[
+            styles.tabButton,
+            activeTab === 'Tab4' && styles.activeTab,
+          ]}
+          onPress={() => switchTab('Tab4')}
+        >
+          <Text>Tab 4</Text>
+        </TouchableOpacity>
+        </View>
+
+         {/* Content based on active tab */}
+        <View style={styles.tabContent}>
+            <Text>{tabContent[activeTab]}</Text>
+        </View>
+
       </View>
 
       <View style={styles.bottomProfile}>
@@ -50,30 +171,81 @@ const styles = StyleSheet.create({
         justifyContent: "center",
     },
     topProfile: {
-        flex: 1, 
+        flex: 2, 
         width: "100%",
         backgroundColor: 'lightblue', 
-        justifyContent: 'center', 
-        alignItems: 'center', 
-      },
-      bottomProfile: {
-        flex: 1, 
-        width: "100%",
-        backgroundColor: 'lightgreen', 
         justifyContent: 'center', 
         alignItems: 'center', 
       },
       buttonContainer: {
         flexDirection: 'row',
         justifyContent: 'space-around',
+        marginTop: 10,
       },
       button: {
         backgroundColor: 'grey', 
         padding: 10, 
         borderRadius: 5, 
       },
+      buttonBio:{
+        backgroundColor: 'grey',
+        padding: 5,
+        borderRadius: 5,
+      },
       buttonText: {
         color: 'white', 
         fontWeight: 'bold', 
+      },
+      dropdownButtonText: {
+        fontSize: 16,
+        padding: 10,
+      },
+      dropdownOptionText: {
+        fontSize: 16,
+        padding: 10,
+      },
+      dropdownContainer: {
+        borderWidth: 1,
+        borderColor: 'gray',
+        borderRadius: 5,
+        backgroundColor: 'white',
+      },
+      followingContainer: {
+        flexDirection: 'row', 
+        justifyContent:'center',
+        width: "60%",
+      },
+      section: {
+        flex: 1, 
+        alignItems: 'center',
+      },
+      tabRow: {
+        flexDirection: 'row',
+        marginTop: 10,
+        marginBottom: 10,
+      },
+      tabButton: {
+        flex: 1,
+        padding: 10,
+        alignItems: 'center',
+        borderWidth: 1,
+        borderColor: 'gray',
+      },
+      activeTab: {
+        backgroundColor: 'lightblue',
+      },
+      tabContent: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+
+      // bottom section
+      bottomProfile: {
+        flex: 0.5, 
+        width: "100%",
+        backgroundColor: 'lightgreen', 
+        justifyContent: 'center', 
+        alignItems: 'center', 
       },
 });
