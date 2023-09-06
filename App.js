4b21388229166a2836ccf3c6923b638998639e06
tan-sd <shengdatan@gmail.com>
2023-09-06 16:59:45 +0800

    Styling for Stack and Tab Navigator

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import HomePage from "./screens/HomePage";
 import ShopPage from "./screens/ShopPage";
 import InboxPage from "./screens/InboxPage";
 import ProfilePage from "./screens/ProfilePage";
+import GlobePage from "./screens/GlobePage";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
@@ -13,7 +14,19 @@ const Tab = createBottomTabNavigator();
 
 function TabNavigator() {
     return (
-        <Tab.Navigator>
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                headerShown: false,
+                tabBarStyle: {
+                    backgroundColor:
+                        route.name === "Profile" ||
+                        route.name === "Shop" ||
+                        route.name === "Inbox"
+                            ? "#252525"
+                            : "#000000",
+                },
+            })}
+        >
             <Tab.Screen
                 name="Home"
                 component={HomePage}
@@ -54,6 +67,16 @@ export default function App() {
                     <Stack.Screen
                         name="Tab"
                         component={TabNavigator}
+                        options={{ headerShown: false }}
+                    />
+                    <Stack.Screen
+                        name="GlobePage"
+                        component={GlobePage}
+                        options={{ headerShown: false }}
+                    />
+                    <Stack.Screen
+                        name="ProfilePage"
+                        component={ProfilePage}
                         options={{ headerShown: false }}
                     />
                 </Stack.Navigator>
