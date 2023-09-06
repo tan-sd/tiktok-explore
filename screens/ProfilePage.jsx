4906e7b1ae9cf3b651419273aaad77a1cbe988f6
tan-sd <shengdatan@gmail.com>
2023-09-06 17:01:42 +0800

    Merge with Adam's code

--- screens/ProfilePage.jsx
+++ screens/ProfilePage.jsx
@@ -1,22 +1,27 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View , TouchableOpacity, ScrollView} from "react-native";
-import ModalDropdown from 'react-native-modal-dropdown';
-import React, { useState } from 'react';
-import Videos from "../Components/Videos"
+import {
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+    ScrollView,
+} from "react-native";
+import ModalDropdown from "react-native-modal-dropdown";
+import React, { useState } from "react";
+import Videos from "../Components/Videos";
 
-export default function ProfilePage() {
-
-    const [selectedUser, setSelectedUser] = useState('Adam Tan');
-    const [accountName, setAccountName] = useState('dancerAdam');
-    const [activeTab, setActiveTab] = useState('Tab1');
+export default function ProfilePage({ navigation }) {
+    const [selectedUser, setSelectedUser] = useState("Adam Tan");
+    const [accountName, setAccountName] = useState("dancerAdam");
+    const [activeTab, setActiveTab] = useState("Tab1");
 
     const userToAccountMap = {
-        'Adam Tan': 'dancerAdam',
-        'Rachel': 'singerRach',
-        'Seth Yap': 'drummerSeth',
-        'Sheng Da': 'guitarCaleb',
-        'Jun Sui': 'dancerJun',
-      };
+        "Adam Tan": "dancerAdam",
+        Rachel: "singerRach",
+        "Seth Yap": "drummerSeth",
+        "Sheng Da": "guitarCaleb",
+        "Jun Sui": "dancerJun",
+    };
 
     const handleSelectUser = (index, value) => {
         setSelectedUser(value);
@@ -26,10 +31,10 @@ export default function ProfilePage() {
 
     // Define the content for each tab
     const tabContent = {
-        Tab1: 'Videos',
-        Tab2: 'Private Videos',
-        Tab3: 'Collections',
-        Tab4: 'Heart shape',
+        Tab1: "Videos",
+        Tab2: "Private Videos",
+        Tab3: "Collections",
+        Tab4: "Heart shape",
     };
 
     // Function to switch between tabs
@@ -38,128 +43,128 @@ export default function ProfilePage() {
     };
 
     return (
-    <View style={styles.container}>
-            
-      <View style={styles.topProfile}>
+        <View style={styles.container}>
+            <View style={styles.topProfile}>
+                <ModalDropdown
+                    options={[
+                        "Adam Tan",
+                        "Rachel",
+                        "Seth Yap",
+                        "Sheng Da",
+                        "Jun Sui",
+                    ]}
+                    dropdownTextStyle={styles.dropdownOptionText}
+                    dropdownStyle={styles.dropdownContainer}
+                    onSelect={(index, value) => {
+                        setSelectedUser(value);
+                        const correspondingAccountName =
+                            userToAccountMap[value];
+                        setAccountName(correspondingAccountName);
+                    }}
+                    dropdownPosition="top"
+                >
+                    <Text style={styles.dropdownButtonText}>
+                        {selectedUser ? selectedUser : "Adam Tan"}
+                    </Text>
+                </ModalDropdown>
 
-        <ModalDropdown
-            options={['Adam Tan', 'Rachel', 'Seth Yap', 'Sheng Da', 'Jun Sui', ]} 
-            dropdownTextStyle={styles.dropdownOptionText}
-            dropdownStyle={styles.dropdownContainer}
-            onSelect={(index, value) => {
-            
-                setSelectedUser(value);
-                const correspondingAccountName = userToAccountMap[value];
-                setAccountName(correspondingAccountName);
+                {/* <Icon></Icon> */}
+                <Text>@ {accountName ? accountName : "dancerAdam"}</Text>
+                <View style={styles.followingContainer}>
+                    <View style={styles.section}>
+                        <Text>50</Text>
+                        <Text>Following</Text>
+                    </View>
+                    <View style={styles.section}>
+                        <Text>50</Text>
+                        <Text>Followers</Text>
+                    </View>
+                    <View style={styles.section}>
+                        <Text>50</Text>
+                        <Text>Likes</Text>
+                    </View>
+                </View>
 
-            }}
-            dropdownPosition="top" 
-        >
-        <Text style={styles.dropdownButtonText}>
-          {selectedUser ? selectedUser : 'Adam Tan'}
-        </Text>
-        </ModalDropdown>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity
+                        style={[styles.button, { marginRight: 10 }]}
+                        onPress={() => {
+                            // Handle the press of Button 1
+                        }}
+                    >
+                        <Text style={styles.buttonText}> Edit Profile</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={() => {
+                            // Handle the press of Button 2
+                        }}
+                    >
+                        <Text style={styles.buttonText}>Add Friends</Text>
+                    </TouchableOpacity>
+                </View>
 
-       
-        {/* <Icon></Icon> */}
-        <Text>@ {accountName ? accountName : 'dancerAdam'}</Text>
-        <View style={styles.followingContainer}>
-            <View style={styles.section}>
-                <Text>50</Text>
-                <Text>Following</Text>
+                {/* bio part */}
+                <TouchableOpacity
+                    style={[styles.buttonBio, { marginTop: 10 }]}
+                    onPress={() => {}}
+                >
+                    <Text style={[styles.buttonText, { fontSize: 10 }]}>
+                        Add Bio
+                    </Text>
+                </TouchableOpacity>
+
+                {/* Row of tabs */}
+                <View style={styles.tabRow}>
+                    <TouchableOpacity
+                        style={[
+                            styles.tabButton,
+                            activeTab === "Tab1" && styles.activeTab,
+                        ]}
+                        onPress={() => switchTab("Tab1")}
+                    >
+                        <Text>aaaa</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={[
+                            styles.tabButton,
+                            activeTab === "Tab2" && styles.activeTab,
+                        ]}
+                        onPress={() => switchTab("Tab2")}
+                    >
+                        <Text>Tab 2</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={[
+                            styles.tabButton,
+                            activeTab === "Tab3" && styles.activeTab,
+                        ]}
+                        onPress={() => switchTab("Tab3")}
+                    >
+                        <Text>Tab 3</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={[
+                            styles.tabButton,
+                            activeTab === "Tab4" && styles.activeTab,
+                        ]}
+                        onPress={() => navigation.navigate("GlobePage")}
+                    >
+                        <Text>Tab 4</Text>
+                    </TouchableOpacity>
+                </View>
+
+                {/* Content based on active tab */}
+                <View style={styles.tabContent}>
+                    {/* Idk how to make this load conditionally based on tab clicked: tabContent[activeTab] */}
+                    <ScrollView style={styles.tabContentScroll}>
+                        <Videos numberOfVids={40} />
+                    </ScrollView>
+                </View>
             </View>
-            <View style={styles.section}>
-                <Text>50</Text>
-                <Text>Followers</Text>
-                </View>
-            <View style={styles.section}>
-                <Text>50</Text>
-                <Text>Likes</Text>
-                </View>
-        </View>
 
-        <View style={styles.buttonContainer}>
-            <TouchableOpacity
-                style={[styles.button, {marginRight:10}]}
-                onPress={() => {
-                // Handle the press of Button 1
-                }}
-            >
-            <Text style={styles.buttonText}> Edit Profile</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-                style={styles.button}
-                onPress={() => {
-                // Handle the press of Button 2
-                }}
-            > 
-            <Text style={styles.buttonText}>Add Friends</Text>
-            </TouchableOpacity>
-        </View>
-
-        {/* bio part */}
-        <TouchableOpacity
-            style={[styles.buttonBio, {marginTop: 10}]}
-            onPress={() => {
-            }}
-            > 
-            <Text style={[styles.buttonText, {fontSize:10}]}>Add Bio</Text>
-      </TouchableOpacity>
-
-
-       {/* Row of tabs */}
-       <View style={styles.tabRow}>
-        <TouchableOpacity
-          style={[
-            styles.tabButton,
-            activeTab === 'Tab1' && styles.activeTab,
-          ]}
-          onPress={() => switchTab('Tab1')}
-        >
-          <Text>aaaa</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={[
-            styles.tabButton,
-            activeTab === 'Tab2' && styles.activeTab,
-          ]}
-          onPress={() => switchTab('Tab2')}
-        >
-          <Text>Tab 2</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={[
-            styles.tabButton,
-            activeTab === 'Tab3' && styles.activeTab,
-          ]}
-          onPress={() => switchTab('Tab3')}
-        >
-          <Text>Tab 3</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={[
-            styles.tabButton,
-            activeTab === 'Tab4' && styles.activeTab,
-          ]}
-          onPress={() => switchTab('Tab4')}
-        >
-          <Text>Tab 4</Text>
-        </TouchableOpacity>
-        </View>
-
-         {/* Content based on active tab */}
-        <View style={styles.tabContent}>
-
-          {/* Idk how to make this load conditionally based on tab clicked: tabContent[activeTab] */}
-          <ScrollView style={styles.tabContentScroll}>
-            <Videos numberOfVids={40} />
-          </ScrollView>
-
-        </View>
-      </View>
-    
             <StatusBar style="auto" />
-    </View>
+        </View>
     );
 }
 
@@ -171,85 +176,85 @@ const styles = StyleSheet.create({
         justifyContent: "center",
     },
     topProfile: {
-        flex: 2, 
+        flex: 2,
         width: "100%",
-        backgroundColor: 'lightblue', 
-        justifyContent: 'center', 
-        alignItems: 'center', 
-      },
-      buttonContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
+        backgroundColor: "lightblue",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    buttonContainer: {
+        flexDirection: "row",
+        justifyContent: "space-around",
         marginTop: 10,
-      },
-      button: {
-        backgroundColor: 'grey', 
-        padding: 10, 
-        borderRadius: 5, 
-      },
-      buttonBio:{
-        backgroundColor: 'grey',
+    },
+    button: {
+        backgroundColor: "grey",
+        padding: 10,
+        borderRadius: 5,
+    },
+    buttonBio: {
+        backgroundColor: "grey",
         padding: 5,
         borderRadius: 5,
-      },
-      buttonText: {
-        color: 'white', 
-        fontWeight: 'bold', 
-      },
-      dropdownButtonText: {
+    },
+    buttonText: {
+        color: "white",
+        fontWeight: "bold",
+    },
+    dropdownButtonText: {
         fontSize: 16,
         padding: 10,
-      },
-      dropdownOptionText: {
+    },
+    dropdownOptionText: {
         fontSize: 16,
         padding: 10,
-      },
-      dropdownContainer: {
+    },
+    dropdownContainer: {
         borderWidth: 1,
-        borderColor: 'gray',
+        borderColor: "gray",
         borderRadius: 5,
-        backgroundColor: 'white',
-      },
-      followingContainer: {
-        flexDirection: 'row', 
-        justifyContent:'center',
+        backgroundColor: "white",
+    },
+    followingContainer: {
+        flexDirection: "row",
+        justifyContent: "center",
         width: "60%",
-      },
-      section: {
-        flex: 1, 
-        alignItems: 'center',
-      },
-      tabRow: {
-        flexDirection: 'row',
+    },
+    section: {
+        flex: 1,
+        alignItems: "center",
+    },
+    tabRow: {
+        flexDirection: "row",
         marginTop: 10,
         marginBottom: 0,
-      },
-      tabButton: {
+    },
+    tabButton: {
         flex: 1,
         padding: 10,
-        alignItems: 'center',
+        alignItems: "center",
         borderWidth: 1,
-        borderColor: 'gray',
-      },
-      activeTab: {
-        backgroundColor: 'lightblue',
-      },
-      tabContent: {
+        borderColor: "gray",
+    },
+    activeTab: {
+        backgroundColor: "lightblue",
+    },
+    tabContent: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '100%',
-      },
-      tabContentScroll: {
-        width: '100%',
-      },
-
-      // bottom section
-      bottomProfile: {
-        flex: 0.5, 
+        justifyContent: "center",
+        alignItems: "center",
         width: "100%",
-        backgroundColor: 'lightgreen', 
-        justifyContent: 'center', 
-        alignItems: 'center', 
-      },
+    },
+    tabContentScroll: {
+        width: "100%",
+    },
+
+    // bottom section
+    bottomProfile: {
+        flex: 0.5,
+        width: "100%",
+        backgroundColor: "lightgreen",
+        justifyContent: "center",
+        alignItems: "center",
+    },
 });
